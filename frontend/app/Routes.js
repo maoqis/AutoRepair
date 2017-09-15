9d54fc21de3dc7d5806702c590cc5a0f54b1a29b
Sabbir-Ahmed <sabbir@toptal.com>
2017-09-15 23:40:16 +0200

    Frontend. Added login page and route
    Added login page and styles
    Added a route /login for that page

--- frontend/app/Routes.js
+++ frontend/app/Routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import App from './components/App';
+import Login from './components/Login';
 
 export default () =>
   (<Switch>
     <Route path="/" exact component={App} />
+    <Route path="/login" exact component={Login} />
   </Switch>);
